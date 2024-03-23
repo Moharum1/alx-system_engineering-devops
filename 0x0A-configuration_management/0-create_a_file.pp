e3347d22f4edffcc4539f3cde7637296dc67c864
Moharum1 <moharum4@gamil.com>
2024-03-23 14:16:03 +0200

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file in /tmp
 
 file{ '/tmp/school':
-  ensure => file,
-  content => 'I love Puppet',
-  group => 'www-data',
-  owner => 'www-data',
-  mode => '0744'
+    ensure  => file,
+    content => 'I love Puppet',
+    group   => 'www-data',
+    owner   => 'www-data',
+    mode    => '0744'
 }
